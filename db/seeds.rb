f32c5def7d9c2f6218e55c975dc88a0b2f351fec
juliana geller <bablouchka@gmail.com>
2019-11-07 08:27:20 +0100

    simple airbnb with come css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Basement in Norrebro',
+  address: 'Ahornsgade',
+  description: 'A basement with one bathroom and no kitchen, loads of alcohol and nerds',
+  price_per_night: 55,
+  number_of_guests: 14,
+  picture: "https://images.unsplash.com/photo-1550577406-9db79c15ff5e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1655&q=80"
+)
+
+Flat.create!(
+  name: 'Draculas Castle',
+  address: 'Transilvania',
+  description: 'Nice and quainty castle on top of a hill',
+  price_per_night: 95,
+  number_of_guests: 5,
+  picture: "https://images.unsplash.com/photo-1514539079130-25950c84af65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1649&q=80"
+)
+
+Flat.create!(
+  name: 'Nautilus',
+  address: 'Twenty Thousand Leagues Under the Sea',
+  description: 'Futuristic submarine',
+  price_per_night: 85,
+  number_of_guests: 3,
+  picture: "https://images.unsplash.com/photo-1545289602-6c9479945c49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+)
